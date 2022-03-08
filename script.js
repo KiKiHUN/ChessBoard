8564575f9ace2135e3440c23e5e98a7295bdb567
KiKiHUN1 <71247943+KiKiHUN1@users.noreply.github.com>
2022-03-08 13:07:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    var lepesek = []
+
+
+    $("#pos").click(function(){
+        var babu =  $('#pos').attr('class').split(' ');
+        console.log("Jelenlegi bábu stat: "+babu[2]);
+    });
+
+
+});
